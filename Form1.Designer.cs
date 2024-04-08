66a996db2f98609b12c60a0ca3d78a72c6cc56dd
samedyolchuyev@outlook.com <samedyolchuyev@outlook.com>
2024-04-08 11:14:18 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace WinFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            faiz = new Button();
+            cixma = new Button();
+            vurma = new Button();
+            bolme = new Button();
+            equales = new Button();
+            toplama = new Button();
+            backspaceButton = new Button();
+            clearButton = new Button();
+            screen = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(8, 275);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 53);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlDarkDark;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(89, 275);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 53);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlDarkDark;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(170, 275);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 53);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ControlDarkDark;
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(8, 331);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 53);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ControlDarkDark;
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(89, 331);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 53);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ControlDarkDark;
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Location = new Point(170, 331);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 53);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ControlDarkDark;
+            button7.ForeColor = SystemColors.ButtonHighlight;
+            button7.Location = new Point(8, 390);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 55);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ControlDarkDark;
+            button8.ForeColor = SystemColors.ButtonHighlight;
+            button8.Location = new Point(89, 390);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 55);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ControlDarkDark;
+            button9.ForeColor = SystemColors.ButtonHighlight;
+            button9.Location = new Point(170, 390);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 55);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.BackColor = SystemColors.ControlDarkDark;
+            button0.ForeColor = SystemColors.ButtonHighlight;
+            button0.Location = new Point(89, 214);
+            button0.Name = "button0";
+            button0.Size = new Size(75, 55);
+            button0.TabIndex = 9;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = false;
+            button0.Click += button0_Click;
+            // 
+            // faiz
+            // 
+            faiz.BackColor = SystemColors.ControlDarkDark;
+            faiz.ForeColor = SystemColors.ButtonHighlight;
+            faiz.Location = new Point(89, 153);
+            faiz.Name = "faiz";
+            faiz.Size = new Size(75, 55);
+            faiz.TabIndex = 14;
+            faiz.Text = "%";
+            faiz.UseVisualStyleBackColor = false;
+            faiz.Click += faiz_Click;
+            // 
+            // cixma
+            // 
+            cixma.BackColor = SystemColors.ControlDarkDark;
+            cixma.ForeColor = SystemColors.ButtonHighlight;
+            cixma.Location = new Point(170, 214);
+            cixma.Name = "cixma";
+            cixma.Size = new Size(75, 55);
+            cixma.TabIndex = 15;
+            cixma.Text = "-";
+            cixma.UseVisualStyleBackColor = false;
+            cixma.Click += cixma_Click;
+            // 
+            // vurma
+            // 
+            vurma.BackColor = SystemColors.ControlDarkDark;
+            vurma.ForeColor = SystemColors.ButtonHighlight;
+            vurma.Location = new Point(8, 153);
+            vurma.Name = "vurma";
+            vurma.Size = new Size(75, 55);
+            vurma.TabIndex = 16;
+            vurma.Text = "*";
+            vurma.UseVisualStyleBackColor = false;
+            vurma.Click += vurma_Click;
+            // 
+            // bolme
+            // 
+            bolme.BackColor = SystemColors.ControlDarkDark;
+            bolme.ForeColor = SystemColors.ButtonHighlight;
+            bolme.Location = new Point(170, 153);
+            bolme.Name = "bolme";
+            bolme.Size = new Size(75, 55);
+            bolme.TabIndex = 17;
+            bolme.Text = "/";
+            bolme.UseVisualStyleBackColor = false;
+            bolme.Click += bolme_Click;
+            // 
+            // equales
+            // 
+            equales.BackColor = Color.DarkOrange;
+            equales.Cursor = Cursors.Hand;
+            equales.ForeColor = SystemColors.ButtonHighlight;
+            equales.Location = new Point(251, 275);
+            equales.Name = "equales";
+            equales.Size = new Size(75, 173);
+            equales.TabIndex = 19;
+            equales.Text = "=";
+            equales.UseVisualStyleBackColor = false;
+            equales.Click += equales_Click;
+            // 
+            // toplama
+            // 
+            toplama.BackColor = SystemColors.ControlDarkDark;
+            toplama.ForeColor = SystemColors.ButtonHighlight;
+            toplama.Location = new Point(8, 214);
+            toplama.Name = "toplama";
+            toplama.Size = new Size(75, 55);
+            toplama.TabIndex = 18;
+            toplama.Text = "+";
+            toplama.UseVisualStyleBackColor = false;
+            toplama.Click += toplama_Click;
+            // 
+            // backspaceButton
+            // 
+            backspaceButton.BackColor = SystemColors.ControlDarkDark;
+            backspaceButton.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            backspaceButton.ForeColor = SystemColors.ButtonHighlight;
+            backspaceButton.Location = new Point(251, 153);
+            backspaceButton.Name = "backspaceButton";
+            backspaceButton.Size = new Size(75, 55);
+            backspaceButton.TabIndex = 24;
+            backspaceButton.Text = "Backspace";
+            backspaceButton.UseVisualStyleBackColor = false;
+            backspaceButton.Click += backspaceButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.BackColor = SystemColors.ControlDarkDark;
+            clearButton.Font = new Font("Segoe UI Historic", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            clearButton.ForeColor = Color.Red;
+            clearButton.Location = new Point(251, 214);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(75, 55);
+            clearButton.TabIndex = 25;
+            clearButton.Text = "C";
+            clearButton.UseVisualStyleBackColor = false;
+            clearButton.Click += clearButton_Click;
+            // 
+            // screen
+            // 
+            screen.AutoSize = true;
+            screen.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            screen.ForeColor = SystemColors.ActiveCaptionText;
+            screen.Location = new Point(8, 105);
+            screen.MaximumSize = new Size(315, 40);
+            screen.MinimumSize = new Size(315, 40);
+            screen.Name = "screen";
+            screen.Size = new Size(315, 40);
+            screen.TabIndex = 26;
+            screen.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(335, 450);
+            Controls.Add(screen);
+            Controls.Add(clearButton);
+            Controls.Add(backspaceButton);
+            Controls.Add(equales);
+            Controls.Add(toplama);
+            Controls.Add(bolme);
+            Controls.Add(vurma);
+            Controls.Add(cixma);
+            Controls.Add(faiz);
+            Controls.Add(button0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = Color.Red;
+            Name = "Form1";
+            RightToLeft = RightToLeft.No;
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button faiz;
+        private Button cixma;
+        private Button vurma;
+        private Button bolme;
+        private Button equales;
+        private Button toplama;
+        private Button backspaceButton;
+        private Button clearButton;
+        private Label screen;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System.Media;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        int num1 , num2;
+        int netice;
+        char op;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected void check(int num)
+        {
+            if (screen.Text.Length <= 24)
+            {
+                screen.Text += num;
+            }
+            else
+            {
+                SoundPlayer soundPlayer = new SoundPlayer();
+                soundPlayer.Play();
+            }
+        }
+        protected void check_op(char op)
+        {
+            if (this.op == '+' || this.op == '-' || this.op == '/' || this.op == '*' || this.op == '%')
+            {
+                MessageBox.Show(" Artıq operator seçmisiniz ");
+            }
+            this.op = op;
+            if (screen.Text == "" || screen.Text == " ")
+            {
+                SoundPlayer sound = new SoundPlayer();
+                sound.Play();
+                
+            }
+            else
+            {
+                num1 = int.Parse(screen.Text);
+                screen.Text = "";
+            }
+        }
+        private void button0_Click(object sender, EventArgs e)
+        {
+            check(0);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check(1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            check(2);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            check(3);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            check(4);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            check(5);
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            check(6);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            check(7);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            check(8);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            check(9);
+        }
+        private void toplama_Click(object sender, EventArgs e)
+        {
+            check_op('+');
+        }
+        private void cixma_Click(object sender, EventArgs e)
+        {
+            check_op('-');
+        }
+        private void bolme_Click(object sender, EventArgs e)
+        {
+            check_op('/');
+        }
+        private void vurma_Click(object sender, EventArgs e)
+        {
+            check_op('*');
+        }
+        private void faiz_Click(object sender, EventArgs e)
+        {
+            check_op('%');
+        }
+        private void equales_Click(object sender, EventArgs e)
+        {
+            num2 =int.Parse(screen.Text.ToString());
+            if (this.op == '+')
+            {
+                netice = num2 + num1;
+                screen.Text = netice.ToString();
+            }
+            else if (this.op == '-')
+            {
+                netice = num2 - num1;
+                screen.Text = netice.ToString();
+            }
+            else if (this.op == '/')
+            {
+                netice = num2 / num1;
+                screen.Text = netice.ToString();
+            }
+            else if (this.op == '*')
+            {
+                netice = num2 * num1;
+                screen.Text = netice.ToString();
+            }
+            else if (this.op == '%')
+            {
+                netice = num1 * num2 / 100;
+                screen.Text = netice.ToString();
+            }
+            this.op = ' ';
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            screen.Text = "";
+        }
+        private void backspaceButton_Click(object sender, EventArgs e)
+        {
+            if (screen.Text.EndsWith(' '))
+            {
+               screen.Text = screen.Text.Remove(screen.Text.Length - 1, 1);
+               screen.Text = screen.Text.Remove(screen.Text.Length - 1, 1);
+               screen.Text = screen.Text.Remove(screen.Text.Length - 1, 1);
+            }
+            else if (screen.Text == "" || screen.Text == " ")
+            {
+                SoundPlayer sound = new SoundPlayer();
+                sound.Play();
+            }
+            else
+                screen.Text.Remove(Text.Length - 1, 1);
+                
+        }
+    }
+}
